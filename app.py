50ef1e6e903419d234aef9b73fdcf1e74a906131
Gaurav Chaturvedi <2gauravc@gmail.com>
2020-08-21 22:49:48 +0800

    img save

--- app.py
+++ app.py
@@ -14,6 +14,15 @@ accelerator.
 To install app dependencies in the runtime container, list them in the requirements.txt file.
 """
 
+def arr_to_img(frame,num):
+    from PIL import Image
+    import numpy as np
+
+    w, h = 512, 512
+    data = np.zeros((h, w, 3), dtype=np.uint8)
+    data[0:256, 0:256] = [255, 0, 0] # red patch in upper left
+    img = Image.fromarray(frame, 'RGB')
+    img.save('my.png')
 
 def main():
     pose_estimator = edgeiq.PoseEstimation("alwaysai/human-pose")
@@ -55,11 +64,14 @@ def main():
                         text.append(str(key_point))
                 #print(results.raw_results)
     
-                streamer.send_data(results.draw_poses(frame), text)
+                #streamer.send_data(results.draw_poses(frame), text)
 
                 fps.update()
                 i+=1
 
+                # write the image
+                arr_to_img(frame, i)
+                
                 if streamer.check_exit():
                     break
     finally:
