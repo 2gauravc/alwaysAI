c8e3055fa023fc4bdd5f07f3830deebbba796615
Gaurav Chaturvedi <2gauravc@gmail.com>
2020-08-22 23:30:34 +0800

    run_1

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import logging
 import time
 import edgeiq
 import numpy as np
+from PIL import Image
 
 """
 Use pose estimation to determine human poses in realtime. Human Pose returns
@@ -14,17 +15,12 @@ accelerator.
 To install app dependencies in the runtime container, list them in the requirements.txt file.
 """
 
-def arr_to_img(frame,num):
-    from PIL import Image
-    import numpy as np
-
-    w, h = 512, 512
-    data = np.zeros((h, w, 3), dtype=np.uint8)
-    data[0:256, 0:256] = [255, 0, 0] # red patch in upper left
+def arr_to_img(run_id,frame,frame_num):
     img = Image.fromarray(frame, 'RGB')
-    img.save('my.png')
+    img.save('raw_images/run_{}_frm_{}.png'.format(run_id,frame_num))
 
 def main():
+    run_id = 1
     pose_estimator = edgeiq.PoseEstimation("alwaysai/human-pose")
     pose_estimator.load(
             engine=edgeiq.Engine.DNN)
@@ -39,40 +35,48 @@ def main():
         with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
                 edgeiq.Streamer() as streamer:
             # Allow Webcam to warm up
-            time.sleep(2.0)
+            time.sleep(5.0)
             fps.start()
-            i = 0
+            i = 0 # Frame count
+            j = 0 # Number of capture count
             # loop detection
             while True:
                 
                 frame = video_stream.read()
-                results = pose_estimator.estimate(frame)
-                # Generate text to display on streamer
-                text = ["Model: {}".format(pose_estimator.model_id)]
-                text.append(
+
+                #Take every 5th frame. Take first 5 instances
+
+                if i % 5 ==0:
+                    results = pose_estimator.estimate(frame)
+                    # Generate text to display on streamer
+                    text = ["Model: {}".format(pose_estimator.model_id)]
+                    text.append(
                         "Inference time: {:1.3f} s".format(results.duration))
-                for ind, pose in enumerate(results.poses):
-                    text.append("Person {}".format(ind))
-                    text.append('-'*10)
-                    text.append("Pose Score")
-                    text.append(str(pose.score))
-                    text.append('-'*10)
-                    text.append("Key Points:")
-                    if (i ==0):
+                    for ind, pose in enumerate(results.poses):
+                        text.append("Person {}".format(ind))
+                        text.append('-'*10)
+                        text.append("Pose Score")
+                        text.append(str(pose.score))
+                        text.append('-'*10)
+                        text.append("Key Points:")
+                    
+                        print ("i={}, j={}\n".format(i,j))
                         print (pose.key_points)
+
                     for key_point in pose.key_points:
                         text.append(str(key_point))
-                #print(results.raw_results)
-    
-                #streamer.send_data(results.draw_poses(frame), text)
+                    streamer.send_data(results.draw_poses(frame), text)
 
-                fps.update()
+                    #write the image to disk 
+                    arr_to_img(run_id,frame, j)
+
+                    fps.update()
+                    j+=1
+
                 i+=1
 
-                # write the image
-                arr_to_img(frame, i)
-                
-                if streamer.check_exit():
+                                
+                if j >=5: #streamer.check_exit():
                     break
     finally:
         fps.stop()
